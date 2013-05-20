a8e94f5bd42de0624c950f8a4b500ef0a9c79fdc
Germano Teixeira de Miranda <germano@ri5.com.br>
2013-05-19 22:02:43 -0300

    adiciona idade a aluno

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,13 @@ class Student < ActiveRecord::Base
   validates :birthdate, :email, :name, :registry, :presence => true
   validates :email, :uniqueness => true
 
+  def age
+    current_date = Date.today
+    years = current_date.year - self.birthdate.year
+    years -= 1 if (self.birthdate.month > current_date.month) || (self.birthdate.month == current_date.month && self.birthdate.day > current_date.day)
+    months = current_date.month - self.birthdate.month
+    months -= 1 if self.birthdate.day > current_date.day
+    {:years => years,:months => months}
+  end
+
 end
